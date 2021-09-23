d9e059bde7bdf39ff3cad3de77e3ee1b8f663212
Jared Ledbetter <jared.ledbetter@outlook.com>
2021-09-23 00:16:46 -0400

    remove unused comment

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-require('fast-text-encoding');  // just require me before use
+require('fast-text-encoding');
 
-const myText = 'Turn me into UTF-8!';
+const myText = 'hello@carbondigital.us';
 
 const buffer = new TextEncoder().encode(myText);
 // buffer is now a Uint8Array of [84, 117, 114, 110, ...]
+
+console.log(buffer);
