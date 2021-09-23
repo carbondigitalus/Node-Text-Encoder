7eea5b1f5ce939948edc13f07cab6a963fdb6fd3
Jared Ledbetter <jared.ledbetter@outlook.com>
2021-09-23 00:19:51 -0400

    update text variable name

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('fast-text-encoding');
 
-const myText = 'hello@carbondigital.us';
+const baseText = 'hello@carbondigital.us';
 
 // buffer is now a Uint8Array of [84, 117, 114, 110, ...]
-const buffer = new TextEncoder().encode(myText);
+const buffer = new TextEncoder().encode(baseText);
 
 
 console.log(buffer);
