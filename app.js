13332e219c7cc8a5cfa941d480277e2770404214
Jared Ledbetter <jared.ledbetter@outlook.com>
2021-09-23 00:18:59 -0400

    moved comment location

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('fast-text-encoding');
 
 const myText = 'hello@carbondigital.us';
 
-const buffer = new TextEncoder().encode(myText);
 // buffer is now a Uint8Array of [84, 117, 114, 110, ...]
+const buffer = new TextEncoder().encode(myText);
+
 
 console.log(buffer);
