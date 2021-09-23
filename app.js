3eb62b5c319cc6f682af59f035bbc521274f96eb
Jared Ledbetter <jared.ledbetter@outlook.com>
2021-09-23 00:11:49 -0400

    add variable for custom text

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require('fast-text-encoding');  // just require me before use
 
-const buffer = new TextEncoder().encode('Turn me into UTF-8!');
+const myText = 'Turn me into UTF-8!';
+
+const buffer = new TextEncoder().encode(myText);
 // buffer is now a Uint8Array of [84, 117, 114, 110, ...]
